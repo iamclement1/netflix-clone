619a039c90f3410851612567bbe92b243d66ce97
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-09-01 18:55:11 +0100

    feats: installed fonts, set global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold uppercase">
-        welcome to netflix
-      </h1>
-    </div>
+    <>
+    </>
   )
 }
 
