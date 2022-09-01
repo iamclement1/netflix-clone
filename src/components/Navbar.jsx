0edb2b4a879ec049ee129250631ad6a4f6714965
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-09-01 19:05:37 +0100

    feats: added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+    return (
+        <div className="flex justify-between p-4 z-[100] items-center w-full absolute">
+            <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+                NETFLIX
+            </h1>
+            <div>
+                <button className="text-white pr-4"> Sign In </button>
+                <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white"> Sign Up </button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
