926601d670ee521e6fdb968dea189af722724809
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-09-06 15:41:04 +0100

    feats: conneted firebase + created login, signup files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
+import { AuthContextProvider } from "./context/AuthContext"
 import Home from "./pages/Home"
 
 function App() {
 
   return (
     <>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={ <Home />} />
-    </Routes>
+    {/* THE NAVBAR IS ADDED COS IT NEEDS TO HAVE ACCESS TO THE CONTEXT CONDITIONAL RENDER WHETHER A USER IS LOGGED IN OR OUT */}
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </AuthContextProvider>
     </>
   )
 }
