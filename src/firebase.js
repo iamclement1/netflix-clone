08c9d483bac9189e0f7e67c293fe5b8548e6b01b
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-09-06 14:57:37 +0100

    feats: firebase auth + context api

--- src/firebase.js
+++ src/firebase.js
@@ -5,6 +5,7 @@ import {
 import {
     getAnalytics
 } from "firebase/analytics";
+import {getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,5 +23,6 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app)
